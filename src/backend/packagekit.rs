132ecff0309bd675db7ffce189eabc05b59e5a79
Jeremy Soller <jackpot51@gmail.com>
2024-01-20 10:59:24 -0700

    locate packages that provide applications from packagekit

--- src/backend/packagekit.rs
+++ src/backend/packagekit.rs
@@ -1,9 +1,10 @@
 use appstream::Collection;
+use cosmic::widget;
 use packagekit_zbus::{
     zbus::blocking::Connection, PackageKit::PackageKitProxyBlocking,
     Transaction::TransactionProxyBlocking,
 };
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
 use super::{Backend, Package};
 
@@ -23,31 +24,44 @@ impl Backend for Packagekit {
     fn installed(&self) -> Result<Vec<Package>, Box<dyn Error>> {
         //TODO: use async?
         let pk = PackageKitProxyBlocking::new(&self.connection)?;
+        //TODO: set locale
         let tx_path = pk.create_transaction()?;
         let tx = TransactionProxyBlocking::builder(&self.connection)
             .destination("org.freedesktop.PackageKit")?
             .path(&tx_path)?
             .build()?;
         let filter_installed = 1 << 2;
-        tx.get_packages(filter_installed)?;
+        let filter_application = 1 << 24;
+        tx.get_packages(filter_installed | filter_application)?;
+        let mut packages = Vec::new();
         for signal in tx.receive_all_signals()? {
             match signal.member() {
                 Some(member) => {
                     if member == "Package" {
                         // https://www.freedesktop.org/software/PackageKit/gtk-doc/Transaction.html#Transaction::Package
                         let (info, package_id, summary) = signal.body::<(u32, &str, &str)>()?;
+                        let mut parts = package_id.split(';');
+                        let name = parts.next().unwrap_or(package_id).to_string();
+                        let version = parts.next().unwrap_or_default().to_string();
                         println!("{} {}: {}", info, package_id, summary);
+                        packages.push(Package {
+                            id: package_id.to_string(),
+                            //TODO: get icon from appstream data?
+                            icon: widget::icon::from_name("package-x-generic"),
+                            name,
+                            version,
+                            extra: HashMap::new(),
+                        })
                     } else if member == "Finished" {
                         break;
+                    } else {
+                        println!("unknown signal {}", member);
                     }
                 }
                 None => {}
             }
         }
-        //TODO
-        //let packages = tx.receive_packages()?;
-        //println!("{:?}", packages);
-        Err("unimplemented".into())
+        Ok(packages)
     }
 
     fn appstream(&self, package: &Package) -> Result<Collection, Box<dyn Error>> {
