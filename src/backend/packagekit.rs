5ed36c2f70b3fe8b1d506c59ac338c7a5c0ece4c
Jeremy Soller <jackpot51@gmail.com>
2024-03-12 21:02:49 -0600

    Partial implementation of packagekit install/uninstall

--- src/backend/packagekit.rs
+++ src/backend/packagekit.rs
@@ -7,11 +7,75 @@ use std::{collections::HashMap, error::Error};
 use super::{Backend, Package};
 use crate::{AppInfo, AppstreamCache, OperationKind};
 
-// https://lazka.github.io/pgi-docs/PackageKitGlib-1.0/enums.html#PackageKitGlib.FilterEnm
+struct TransactionPackage {
+    info: u32,
+    package_id: String,
+    summary: String,
+}
+
+struct TransactionProgress {
+    package_id: String,
+    status: u32,
+    percentage: u32,
+}
+
+fn transaction_handle(
+    tx: TransactionProxyBlocking,
+    mut on_progress: impl FnMut(TransactionProgress),
+) -> Result<Vec<TransactionPackage>, Box<dyn Error>> {
+    let mut packages = Vec::new();
+    for signal in tx.receive_all_signals()? {
+        match signal.member() {
+            Some(member) => match member.as_str() {
+                "ErrorCode" => {
+                    // https://www.freedesktop.org/software/PackageKit/gtk-doc/Transaction.html#Transaction::ErrorCode
+                    let (code, details) = signal.body::<(u32, String)>()?;
+                    return Err(format!("{details} (code {code})").into());
+                }
+                "ItemProgress" => {
+                    // https://www.freedesktop.org/software/PackageKit/gtk-doc/Transaction.html#Transaction::ItemProgress
+                    let (package_id, status, percentage) = signal.body::<(String, u32, u32)>()?;
+                    on_progress(TransactionProgress {
+                        package_id,
+                        status,
+                        percentage,
+                    })
+                }
+                "Package" => {
+                    // https://www.freedesktop.org/software/PackageKit/gtk-doc/Transaction.html#Transaction::Package
+                    let (info, package_id, summary) = signal.body::<(u32, String, String)>()?;
+                    packages.push(TransactionPackage {
+                        info,
+                        package_id,
+                        summary,
+                    });
+                }
+                "Finished" => {
+                    break;
+                }
+                _ => {
+                    log::warn!("unknown signal {}", member);
+                }
+            },
+            None => {}
+        }
+    }
+    Ok(packages)
+}
+
+// https://lazka.github.io/pgi-docs/PackageKitGlib-1.0/enums.html#PackageKitGlib.FilterEnum
 #[repr(u64)]
 enum FilterKind {
     None = 1 << 1,
     Installed = 1 << 2,
+    Newest = 1 << 16,
+    Arch = 1 << 18,
+}
+
+#[repr(u64)]
+enum TransactionFlag {
+    None = 1 << 0,
+    OnlyTrusted = 1 << 1,
 }
 
 #[derive(Debug)]
@@ -46,30 +110,14 @@ impl Packagekit {
         &self,
         tx: TransactionProxyBlocking,
     ) -> Result<Vec<Package>, Box<dyn Error>> {
-        let mut package_ids = Vec::new();
-        for signal in tx.receive_all_signals()? {
-            match signal.member() {
-                Some(member) => {
-                    if member == "Package" {
-                        // https://www.freedesktop.org/software/PackageKit/gtk-doc/Transaction.html#Transaction::Package
-                        let (_info, package_id, _summary) =
-                            signal.body::<(u32, String, String)>()?;
-                        package_ids.push(package_id);
-                    } else if member == "Finished" {
-                        break;
-                    } else {
-                        log::warn!("unknown signal {}", member);
-                    }
-                }
-                None => {}
-            }
-        }
-        drop(tx);
+        let tx_packages = transaction_handle(tx, |_| {})?;
 
         let mut packages = Vec::new();
-        for package_id in package_ids {
-            let mut parts = package_id.split(';');
-            let package_name = parts.next().unwrap_or(&package_id);
+        for tx_package in tx_packages {
+            let mut parts = tx_package.package_id.split(';');
+            let Some(package_name) = parts.next() else {
+                continue;
+            };
             let version_opt = parts.next();
             let _architecture_opt = parts.next();
 
@@ -134,10 +182,65 @@ impl Backend for Packagekit {
     fn operation(
         &self,
         kind: OperationKind,
-        _package_id: &str,
-        _info: &AppInfo,
-        _f: Box<dyn FnMut(f32) + 'static>,
+        package_id: &str,
+        info: &AppInfo,
+        mut f: Box<dyn FnMut(f32) + 'static>,
     ) -> Result<(), Box<dyn Error>> {
-        Err(format!("{kind:?} not implemented for packagekit backend").into())
+        let Some(pkgname) = &info.pkgname else {
+            return Err(format!("{} missing package name", package_id).into());
+        };
+        let tx_packages = {
+            let tx = self.transaction()?;
+            tx.resolve(
+                FilterKind::Newest as u64 | FilterKind::Arch as u64,
+                &[pkgname],
+            )?;
+            transaction_handle(tx, |_| {})?
+        };
+        let mut package_ids = Vec::new();
+        for tx_package in tx_packages.iter() {
+            package_ids.push(tx_package.package_id.as_str());
+        }
+        match kind {
+            OperationKind::Install => {
+                log::info!("installing packages {:?}", package_ids);
+                {
+                    let tx = self.transaction()?;
+                    tx.set_hints(&["interactive=true"])?;
+                    tx.install_packages(TransactionFlag::OnlyTrusted as u64, &package_ids)?;
+                    let _tx_packages = transaction_handle(tx, |progress| {
+                        log::info!(
+                            "{} {} {}%",
+                            progress.package_id,
+                            progress.status,
+                            progress.percentage
+                        );
+                        //TODO: show progress as total of all items
+                        f(progress.percentage as f32);
+                    })?;
+                }
+                Ok(())
+            }
+            OperationKind::Uninstall => {
+                log::info!("uninstalling packages {:?}", package_ids);
+                {
+                    let tx = self.transaction()?;
+                    tx.set_hints(&["interactive=true"])?;
+                    //TODO: transaction flags, autoremove?
+                    tx.remove_packages(0, &package_ids, true, false)?;
+                    let _tx_packages = transaction_handle(tx, |progress| {
+                        log::info!(
+                            "{} {} {}%",
+                            progress.package_id,
+                            progress.status,
+                            progress.percentage
+                        );
+                        //TODO: show progress as total of all items
+                        f(progress.percentage as f32);
+                    })?;
+                }
+                Ok(())
+            }
+        }
     }
 }
