ecc50203beb91124b1e99bd8194b8de18cb5b3cf
Jeremy Soller <jackpot51@gmail.com>
2024-04-19 10:26:59 -0600

    Add custom priorities for lutris and steam

--- src/priority.rs
+++ src/priority.rs
@@ -1,11 +1,24 @@
 //TODO: less hard-coded rules for repo priorities
-pub fn priority(backend_name: &str, source_id: &str, _id: &str) -> i32 {
+pub fn priority(backend_name: &str, source_id: &str, id: &str) -> i32 {
     let mut priority = 0;
-    if backend_name == "flatpak" {
-        priority += 2;
-    }
-    if source_id == "flathub" {
-        priority += 1;
+    match id {
+        // These ids prefer the packagekit backend
+        "net.lutris.Lutris" | "com.valvesoftware.Steam" => {
+            if backend_name == "packagekit" {
+                priority += 2;
+            }
+        }
+        // All other sources prefer the flatpak backend
+        _ => {
+            if backend_name == "flatpak" {
+                priority += 2;
+
+                // Among flatpak sources, the flathub source is preferred
+                if source_id == "flathub" {
+                    priority += 1;
+                }
+            }
+        }
     }
     priority
 }
