c25a13ce9e35db60e9df5f6da637855dcdfa0122
Jeremy Soller <jackpot51@gmail.com>
2024-06-05 09:35:16 -0600

    Fix compilation of libflatpak example

--- examples/libflatpak.rs
+++ examples/libflatpak.rs
@@ -1,11 +1,11 @@
-use libflatpak::{gio::Cancellable, prelude::*, Installation, RefKind, Transaction};
+use libflatpak::{gio::Cancellable, prelude::*, Installation, Transaction};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let inst = Installation::new_user(Cancellable::NONE)?;
     let tx = Transaction::for_installation(&inst, Cancellable::NONE)?;
     tx.connect_new_operation(|_, op, progress| {
-        println!("Operation: {} {:?}", op.operation_type(), op.get_ref());
+        println!("Operation: {:?} {:?}", op.operation_type(), op.get_ref());
         progress.connect_changed(|progress| {
             println!("Progress: {}", progress.progress());
         });
